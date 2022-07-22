91d1d676bb2f31ce6745bf49221da5de2723a5a6
8608858 <ZX123qwe>
2022-07-22 09:44:07 +0800

    update

--- cttqFuncs/conn/minioFuncs.py
+++ cttqFuncs/conn/minioFuncs.py
@@ -3,12 +3,15 @@ Author: Logic
 Date: 2022-05-23 14:33:08
 LastEditTime: 2022-05-27 11:47:22
 FilePath: \PyShovel\impalaTableCheck\minioFuncs.py
-Description: 
+Description:
 '''
 
 from minio import Minio as MinioConnect
 from minio.error import S3Error
+from minio.datatypes import Object as MinioObj
 from cttqFuncs.basic.configFunc import getDict
+from typing import List
+import cttqFuncs.basic.exFunc
 
 
 class Minio():
@@ -19,11 +22,23 @@ class Minio():
                                    secure=False)
         self.bucket = config['db']
 
-    def upload(self, name, path, content_type="application/octet-stream"):
-        self.client.fput_object(self.bucket, name, path, content_type)
+    def upload(self, remoteName, localPath, content_type="application/octet-stream", metadata: dict = {}):
+        self.client.fput_object(self.bucket, remoteName, localPath,
+                                content_type, metadata)
 
     def remove(self, name):
         self.client.remove_object(self.bucket, name)
+
+    def download(self, name, path):
+        self.client.fget_object(self.bucket, name, path)
+
+    def listFiles(self, path: str = '') -> List[MinioObj]:
+        files = list(self.client.list_objects(
+            self.bucket, prefix=path,
+            recursive=True,
+            include_user_meta=True))
+
+        return files
 
     @staticmethod
     def fixedMinio(ns='minio'):
