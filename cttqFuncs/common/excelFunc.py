7b6e29c3a5e5d84739e2527faca418d4c303f0bb
8608858 <ZX123qwe>
2022-10-14 17:12:13 +0800

    update

--- cttqFuncs/common/excelFunc.py
+++ cttqFuncs/common/excelFunc.py
@@ -16,19 +16,16 @@ class XlsWriter():
     def __init__(self, path: str) -> None:
         self.path = path
 
-        if path.endswith('xlsx'):
+        if path.endswith('.xlsx'):
             self.isXlsx = True
-            self.isFist = True
-        elif path.endswith('xls'):
+            self.workbook = openpyxl.Workbook(write_only=True)
+        elif path.endswith('.xls'):
             self.isXlsx = False
+            self.workbook = xlwt.Workbook(encoding="utf-8")
         else:
             raise CommonException('文件类型不正确')
 
-        # 实例化book对象
-        if self.isXlsx:
-            self.workbook = openpyxl.Workbook(write_only=True)
-        else:
-            self.workbook = xlwt.Workbook(encoding="utf-8")
+    
 
     def save(self):
         self.workbook.save(self.path)
@@ -37,20 +34,14 @@ class XlsWriter():
                     sheetName: str):
         # 生成sheet
         if self.isXlsx:
-            if self.isFist:
-                sheet = self.workbook.active
-                sheet.title = sheetName
-                self.isFist = False
-            else:
-                sheet = self.workbook.create_sheet(sheetName)
+           
+            sheet = self.workbook.create_sheet(sheetName)
             # 写入标题
-            for col, column in enumerate(headers):
-                sheet.cell(1, col+1, column)
+            sheet.append(headers)
 
             # 写入每一行
-            for row, data in enumerate(datas):
-                for col, col_data in enumerate(data):
-                    sheet.cell(row + 2, col+1, col_data)
+            for data in datas:
+                sheet.append(data)
         else:
 
             sheet = self.workbook.add_sheet(sheetName)
