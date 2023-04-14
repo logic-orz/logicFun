883b447ed23af741cebc72e7c5fe9dbb9c23f3e9
8608858 <1368754736@qq.com>
2023-04-14 13:11:14 +0800

    update

--- cttqFuncs/common/__init__.py
+++ cttqFuncs/common/__init__.py
@@ -1,9 +1,10 @@
-'''
-Author: Logic
-Date: 2022-04-28 10:59:40
-LastEditTime: 2022-06-14 15:28:24
-Description: 
-'''
-
-__all__ = ['cacheFunc', 'dataShow', 'excelFunc',
-           'logFunc', 'scheduleFunc', 'fileFunc','formatTrans','zipFunc']
+__all__ = [
+    'cacheFunc',
+    'dataShow',
+    'excelFunc',
+    'logFunc',
+    'scheduleFunc',
+    'fileFunc',
+    'formatTrans',
+    'zipFunc'
+]
